e1273163f116943a6cdb1ee2c620363feded7bec
ROleP <>
2016-09-26 11:17:50 +0300

    1-6 logging

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -1,18 +1,25 @@
 package ru.javawebinar.topjava.web;
 
+import org.slf4j.Logger;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static org.slf4j.LoggerFactory.*;
+
 /**
  * Created by rolep on 25/09/16.
  */
 public class UserServlet extends HttpServlet {
 
+    private static final Logger LOG = getLogger(UserServlet.class);
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        LOG.debug("redirect to userList");
         //req.getRequestDispatcher("/userList.jsp").forward(req, resp);
         resp.sendRedirect("userList.jsp");
     }
